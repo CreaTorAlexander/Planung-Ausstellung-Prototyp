9c719d07c1c71ff3729fe644f8f3f34635c79084
alexanderhess <Hess.alexander1@web.de>
2022-11-29 17:26:16 +0100

    Changes for the Content

--- server.py
+++ server.py
@@ -7,9 +7,15 @@ app = Flask(__name__, static_folder="static")
 
 # a route where we will display a welcome message via an HTML template
 @app.route("/")
-def hello():
+def index():
     message = "Hello, World"
     return render_template('index.html', message=message)
+
+
+@app.route("/content")
+def content():
+    message = "Hello, World"
+    return render_template('content.html', message=message)
 
 # run the application
 if __name__ == "__main__":
